25cd691c6bca6ec895e76a582d53c62af259ff84
PratyayPande <pratyaypande@gmail.com>
2020-05-22 11:53:06 +0530

    Updated using VS Code

--- Java Code/BinTreeApp.java
+++ Java Code/BinTreeApp.java
@@ -7,7 +7,6 @@ public class BinTreeApp
         BinaryTree ob = new BinaryTree();
         InputStreamReader read = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(read);
-        Scanner sc = new Scanner(System.in);
         System.out.println("Binary Tree Application Console");
         System.out.println("\n\nAn user Friendly Program to implement a binary tree");
         System.out.println("Version no.: 0.0.1");
@@ -18,7 +17,10 @@ public class BinTreeApp
             if(!inp.equals("exit"))
                 ob.command_center(inp);
             else
+            {
+                System.out.println("Exiting Binary Tree. Thank You");
                 stayInTree = false;
+            }
             //System.out.println("");
         }
     }
