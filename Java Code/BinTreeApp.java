144395374d26fa6b13c693e980e1205a7b5a488d
PratyayPande <pratyaypande@gmail.com>
2020-05-22 09:13:40 +0530

    Changed BinTreeApp.java

--- Java Code/BinTreeApp.java
+++ Java Code/BinTreeApp.java
@@ -7,6 +7,7 @@ public class BinTreeApp
         BinaryTree ob = new BinaryTree();
         InputStreamReader read = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(read);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Binary Tree Application Console");
         System.out.println("\n\nAn user Friendly Program to implement a binary tree");
         System.out.println("Version no.: 0.0.1");
