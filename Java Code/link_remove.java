4ee117f43cc89616f9ed9c3c87abb37aa330682b
PratyayPande <pratyaypande@gmail.com>
2020-06-02 08:04:36 +0530

    Backup commit

--- Java Code/link_remove.java
+++ Java Code/link_remove.java
@@ -1,16 +1,21 @@
 import java.io.*;
+import java.util.*;
 // code to remove the repeating nodes in a linked list
-public class link_remove
+public class link_remove extends Link
 {
+    Link first;
+    
     public static void main(String[] args) throws Exception
     {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader in= new BufferedReader(input);
         System.out.print("Enter the number of elements of the linked list: ");
+        link_remove ob = new link_remove();
         int num = Integer.parseInt(in.readLine());
         for(int i=0;i<num;i++)
         {
-            LinkedList ob = new LinkedList(Integer.parseInt(in.readLine()));
+            int nm = Integer.parseInt(in.readLine());
+            ob.insert(nm);
             // accepting the elements of the linked list from the user
         }
         /*
@@ -20,6 +25,10 @@ public class link_remove
          *
          *
          */
+
+        ob.displayList();
+        ob.removeDup();
+        ob.displayList();
     
     }
     void removeDup()
@@ -30,9 +39,48 @@ public class link_remove
             {
                 if(i.getData() == j.next.getData())
                 {
+                    j.next.getData() = 0;
                     j.next = j.next.next;
                 }
             }
+        }
+    }
+    void insert(int num)
+    {
+        Link newnode = new Link(num);
+        newnode.next = first;
+        first = newnode;
+    }
+    void displayList()
+    {
+        System.out.print("NULL");
+        for(LinkedList i=first;i!=null;i=i.next)
+        {
+            System.out.print(" <- " + i.getData());
+        }
+    }
+    boolean checkPresence(Link testnode)
+    {
+        for(Link ob = first;ob.next!=testnode)
+    }
+    int calSize()
+    {
+        
+    }
+    void delCircular()
+    {
+        HashMap<Link,Integer> ob = new HashMap<>();
+        Link ob = first;
+        boolean repeats=false;
+        while(!repeats)
+        {
+            for(Link testnode=first;testnode != ob && !repeats;testnode=testnode.next)
+            {
+                if(testnode == ob.next)
+                    repeats = 
+            }
+            
+
         }
     }
 
